1660bcee8de722b0222da563e38e61ea0095c66a
Raikonif <raikonif@gmail.com>
2024-09-23 17:13:09 -0400

    handle the searcher functionality

--- src/services/storeAPI.ts
+++ src/services/storeAPI.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { BACKEND_URL, LOCAL_BACKEND_URL } from "@/constants/general.constants.ts";
+import { BACKEND_URL } from "@/constants/general.constants.ts";
 
 export const storeAPI = axios.create({
   baseURL: BACKEND_URL,
