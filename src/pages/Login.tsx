7e727d21ac69ea14b47ef5fe920c34480e7df685
Raikonif <raikonif@gmail.com>
2024-11-04 08:22:29 -0400

    login modified

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -58,10 +58,12 @@ function Login() {
   }, [navigate]);
 
   return (
-    <div className="flex min-h-screen w-full items-center justify-center bg-gray-50 px-4 py-12 sm:px-6 lg:px-8">
+    <div className="flex min-h-screen w-full items-center justify-center bg-gray-50 px-4 sm:px-6 md:py-12 lg:px-8">
       <div className="w-full max-w-md space-y-8">
         <div>
-          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Iniciar Sesión</h2>
+          <h2 className="text-center text-3xl font-extrabold text-gray-900 md:mt-6">
+            Iniciar Sesión
+          </h2>
           <p className="mt-2 text-center text-sm text-gray-600">
             Pon tu email e ingresa el codigo enviado a tu correo
           </p>
