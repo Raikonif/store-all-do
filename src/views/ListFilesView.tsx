869bf3a6b3ca88b9a0abf3f1216f7114c457455b
raikonif <raikonif@gmail.com>
2025-01-09 16:13:35 -0400

    fixed deleted item and refactored row table into a new component to handle the multi select

--- src/views/ListFilesView.tsx
+++ src/views/ListFilesView.tsx
@@ -8,6 +8,7 @@ import { IFile, IFolder } from "@/interfaces/DOFileFolder.ts";
 import toast from "react-hot-toast";
 import { MdCheckBox, MdOutlineCheckBoxOutlineBlank } from "react-icons/md";
 import { FaFileArchive, FaFolder } from "react-icons/fa";
+import RowTable from "@/components/RowTable";
 
 interface Props {
   data: (IFile | IFolder)[];
@@ -59,101 +60,7 @@ function ListFilesView({ data }: Props) {
   return (
     <>
       {data && Array.isArray(data) && data.length > 0 ? (
-        data.map((item, index) =>
-          "Prefix" in item ? (
-            <tr
-              key={index}
-              className="border-b border-gray-700 last:border-b-0 hover:bg-gray-800"
-              onClick={() => navigateToFolder(item)}
-            >
-              <td className="flex items-center py-1 text-sm">
-                <div className="flex items-center justify-center">
-                  {isCheckboxChecked ? (
-                    <MdCheckBox
-                      className="mx-2 text-gray-400"
-                      onClick={() => setIsCheckboxChecked(false)}
-                    />
-                  ) : (
-                    <MdOutlineCheckBoxOutlineBlank
-                      className="mx-2 text-gray-400"
-                      onClick={() => setIsCheckboxChecked(true)}
-                    />
-                  )}
-
-                  <FaFolder className="mr-2 text-yellow-500" size={15} />
-                  <span className="text-gray-400">{item.Prefix.slice(0, -1).split("/").pop()}</span>
-                </div>
-              </td>
-              <td className="py-1 text-xs text-gray-400">-</td>
-              <td className="py-1 text-xs text-gray-400">-</td>
-              <td className="py-1 text-xs text-gray-400">
-                <button onClick={(e) => handleOpenDelete(item, e)}>
-                  <Trash className="text-red-500" />
-                </button>
-              </td>
-            </tr>
-          ) : (
-            <tr
-              key={index}
-              className={`${item.Key.endsWith("/") && "cursor-pointer"} border-b border-gray-700 last:border-b-0 hover:bg-gray-800`}
-            >
-              <td className="flex items-center py-3 text-sm">
-                {item.Key.endsWith("/") ? (
-                  <FolderIcon className="mr-2 text-yellow-500" size={15} />
-                ) : (
-                  <>
-                    {isCheckboxChecked ? (
-                      <MdCheckBox
-                        className="mx-2 text-gray-400"
-                        onClick={() => setIsCheckboxChecked(false)}
-                      />
-                    ) : (
-                      <MdOutlineCheckBoxOutlineBlank
-                        className="mx-2 text-gray-400"
-                        onClick={() => setIsCheckboxChecked(true)}
-                      />
-                    )}
-                    <FaFileArchive className="mr-2 text-blue-600" size={15} />
-                  </>
-                )}
-                <span className="text-gray-400">
-                  {item.Key.endsWith("/")
-                    ? item.Key.slice(0, -1).split("/").pop()
-                    : item.Key.split("/").pop()}
-                </span>
-              </td>
-              <td className="py-1 text-xs text-gray-400">
-                {item.Size > 0 && (item.Size / (1024 * 1024)).toFixed(2) + "MB"}
-              </td>
-              <td className="py-1 text-xs text-gray-400">
-                {convertToNaturalDate(item.LastModified)}
-              </td>
-              <td className="py-1 text-xs text-gray-600">
-                {!item.Key.endsWith("/") && (
-                  <div className="flex gap-10">
-                    <button onClick={() => openFile(item)}>
-                      <Eye size={25} className="text-cyan-500" />
-                    </button>
-                    <button onClick={() => handleDownloadFile(item.Key)}>
-                      {/*<a href={tempItem.url} download>*/}
-                      <Download
-                        size={25}
-                        className="text-green-500 hover:text-green-400 active:text-green-300"
-                      />
-                      {/*</a>*/}
-                    </button>
-                    <button onClick={(e) => handleOpenDelete(item, e)}>
-                      <Trash
-                        size={25}
-                        className="text-red-500 hover:text-red-400 active:text-red-300"
-                      />
-                    </button>
-                  </div>
-                )}
-              </td>
-            </tr>
-          ),
-        )
+        data.map((item, index) => <RowTable key={index} item={item} index={index} />)
       ) : (
         <tr>
           <td colSpan={4} className="text-center text-gray-400">
